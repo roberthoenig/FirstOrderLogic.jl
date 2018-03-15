aa997a3da5f44bfa04cd32a2727537fe3c6adaa3
Robert Hönig <robhoenig@gmail.com>
2018-03-14 22:54:37 -0400

    test_quantified_variables_form.jl: Wrap long lines.

--- test/test_quantified_variables_form.jl
+++ test/test_quantified_variables_form.jl
@@ -1,4 +1,6 @@
 @testset "get_quantified_variables_form" begin
-    @customtest String(get_quantified_variables_form(Formula("*{foo}P(foo(foo), foo, bar) & E(baz, bar)"))) == "?{baz}?{bar}(*{foo}P(foo(foo), foo, bar) & E(baz, bar))"
-    @customtest String(get_quantified_variables_form(Formula("*{x}?{y}*{z}?{w}(~P(a, w) | Q(f(x), y))"))) == "?{a}*{x}?{y}*{z}?{w}(~P(a, w) | Q(f(x), y))"
+    @customtest String(get_quantified_variables_form(Formula("*{foo}P(foo(foo), foo, bar) & E(baz, bar)"))) ==
+        "?{baz}?{bar}(*{foo}P(foo(foo), foo, bar) & E(baz, bar))"
+    @customtest String(get_quantified_variables_form(Formula("*{x}?{y}*{z}?{w}(~P(a, w) | Q(f(x), y))"))) ==
+        "?{a}*{x}?{y}*{z}?{w}(~P(a, w) | Q(f(x), y))"
 end
