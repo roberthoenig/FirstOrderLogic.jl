c24e328466e49581628540d052cecba896b13b0c
Robert Hönig <robhoenig@gmail.com>
2018-03-14 22:55:09 -0400

    Enable all tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,11 @@
 using FirstOrderLogic
 using Base.Test
 
-# include("test_parser.jl")
-# include("test_renamed_quantifiers_form.jl")
-# include("test_quantified_variables_form.jl")
-# include("test_prenex_normal_form.jl")
-# include("test_skolem_normal_form.jl")
-# include("test_removed_quantifier_form.jl")
-# include("test_conjunctive_normal_form.jl")
+include("test_parser.jl")
+include("test_renamed_quantifiers_form.jl")
+include("test_quantified_variables_form.jl")
+include("test_prenex_normal_form.jl")
+include("test_skolem_normal_form.jl")
+include("test_removed_quantifier_form.jl")
+include("test_conjunctive_normal_form.jl")
 include("test_resolution.jl")
